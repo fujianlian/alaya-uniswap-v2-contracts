94cd73755f265ed9b0b7cbb8d03ec11a7046267b
fujianlian <binqiang.lian@hub.hashkey.com>
2021-11-24 11:36:12 +0800

    WATP部署

---
+++ migrations/2_initial_migration.js
@@ -0,0 +1,8 @@
+const WATP = artifacts.require("WATP");
+
+module.exports = async function(deployer) {
+  
+  //  await  deployer.deploy(WATP);
+  //  console.log('WATP at:',WATP.address);
+  var watp = 'atp1l26m259qky6ejcs5r2r6l5n8ne03ge0a6qs5lx'
+};
