8b0518053ea7904a4a05bf8cf73c0afc5b2fabda
fujianlian <binqiang.lian@hub.hashkey.com>
2021-11-24 14:29:41 +0800

    v2核心合约部署配置

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,8 +1,17 @@
 const WATP = artifacts.require("WATP");
+const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 
-module.exports = async function(deployer) {
-  
-  //  await  deployer.deploy(WATP);
-  //  console.log('WATP at:',WATP.address);
-  var watp = 'atp1l26m259qky6ejcs5r2r6l5n8ne03ge0a6qs5lx'
+const feeToSetter = 'atp1ay4d53qawvp64myjzgdstppxp4tn3fudwnn89j';
+
+module.exports = async function (deployer) {
+
+  await deployer.deploy(WATP);
+  console.log('WATP at:',WATP.address);
+
+  await deployer.deploy(UniswapV2Factory, feeToSetter);
+  console.log('UniswapV2Factory at:', UniswapV2Factory.address);
+
+  var factory = new web3.platon.Contract(require("../build/contracts/UniswapV2Factory.json")['abi'], UniswapV2Factory.address, { net_type: "atp" });
+  var initHash = await factory.methods.INIT_CODE_PAIR_HASH().call();
+  console.log("initHash is at:", initHash);
 };
